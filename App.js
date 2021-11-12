ff2babbc704d91b7666577c4ef75284abce82a1b
armathur <armathur@iu.edu>
2021-11-11 21:39:51 -0500

    sucessful spotify api call

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { Credentials } from './Credentials';
+import {decode as atob, encode as btoa} from 'base-64'
+
 
 export default function App() {
+  //set atob and btoa as global variables on React Native
+  if (!global.btoa) {
+      global.btoa = btoa;
+  }
+  if (!global.atob) {
+      global.atob = atob;
+  }
+  
+  const spotify = Credentials();
+  const [token, setToken] = useState('');  
+  const [playlistData, setPlaylistData] = useState('');
+
+  useEffect(() => {
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+      setToken(tokenResponse.data.access_token);
+      // const userID = '12176356166';
+      const userID = 'wxddOutbykuddlgj17cep92f'
+      axios(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+      })
+      .then (playlistRaw => {      
+        setPlaylistData(playlistRaw);
+        });
+      });
+  },[spotify.ClientId, spotify.ClientSecret]);
+  console.log(playlistData);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +58,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'f1dcef1720f4432b9dcd4138b84d08fa',
+        ClientSecret: '14d173e683c949868a35ee91ea6e8d3c'
+    }
+}
+
+export { Credentials };
