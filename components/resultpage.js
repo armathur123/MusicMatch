5993082541bdf092b2c27d863c5b36b458e6e7a2
armathur <armathur@iu.edu>
2021-11-19 03:08:25 -0500

    succesfully catching common songs and common song counts

--- components/resultpage.js
+++ components/resultpage.js
@@ -2,15 +2,34 @@ import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Image} f
 import React, {useState, useEffect} from 'react';
 
 const ResultPage = ({songlist1, songlist2}) => {
+
+    useEffect(() => {
+        songCompare();
+        commonSongCatcher();
+    })
+
+    const [commonSongCount, setCommonSongCount] = useState(0);
     const songCompare = () => {
         console.log(songlist1);
         console.log(songlist2);
     }
-    songCompare();
+
+    const commonSongCatcher = () => {
+        let songCount = 0;
+        for (const song1 of songlist1) {
+            for (const song2 of songlist2) {
+                if (song1?.track?.name == song2?.track?.name){
+                    console.log(song1);
+                    songCount += 1;
+                }
+            }
+        }
+        setCommonSongCount(songCount);
+    }
     return (
         <View>
             <Text>Results</Text>
-            <Text></Text>
+            <Text>{commonSongCount}</Text>
         </View>
     );
 }
