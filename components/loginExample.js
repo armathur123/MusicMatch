f2b73228dbfc86993deabf921cbf876001dbb739
armathur <armathur@iu.edu>
2022-01-22 19:48:52 -0500

    successful login and console.log of userRAW data; remember that schema and proxy might have to be used for production, works in expo tho

--- components/loginExample.js
+++ components/loginExample.js
@@ -10,6 +10,7 @@ import {AuthSession} from 'expo';
 
 const loginExample = ({spotify}) => {
 
+    const redirectURI = makeRedirectUri();
     WebBrowser.maybeCompleteAuthSession();
     // Endpoint
     const discovery = {
@@ -25,14 +26,36 @@ const loginExample = ({spotify}) => {
         // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
         // this must be set to false
         usePKCE: false,
-        redirectUri: makeRedirectUri(),
+        redirectUri: redirectURI,
     },
         discovery
     );
 
     useEffect(() => {
         if (response?.type === 'success') {
-            console.log(response)
+            axios('https://accounts.spotify.com/api/token', {
+                headers: {
+                  'Content-Type' : 'application/x-www-form-urlencoded',
+                  'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+                },
+                data: `grant_type=authorization_code&code=${response?.params?.code}&redirect_uri=${redirectURI}`,
+                method: 'POST'
+              }).then((tokenResponse) => {
+                    const usergrab = () => axios(`https://api.spotify.com/v1/me`, { 
+                        method: 'GET',
+                        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token} //requires auth token (tokenresponse)
+                    })
+                    .then(userRAW => {  
+                        console.log(userRAW);
+                    }).catch(err => {
+                        console.log("postToken error");
+                        console.log(err);
+                    });
+                    usergrab();
+              }).catch((err) => {
+                  console.log("token error");
+                  console.log(err);
+              });
         }
     }, [response]);
 
