f2f111cc106719a70e75470fa9216082cf5ff483
abuhelos <abuhelos3@gmail.com>
2022-11-23 16:32:57 +0100

    Fix FlatList Animation

--- components/resultpage.js
+++ components/resultpage.js
@@ -1,11 +1,12 @@
-import { Animated, StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Image} from 'react-native';
-import React, {useState, useEffect, useContext, useMemo} from 'react';
+import { Animated, StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Image, Platform} from 'react-native';
+import React, {useState, useEffect, useContext, useMemo, useRef} from 'react';
 import BasicInfo from '../components/basicInfo';
 import {Dimensions} from 'react-native';
 import { shadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 let themeColor = 'rgb(218,165,32)';
 import { PlaylistDataContext } from '../contexts/PlaylistDataContext';
 import Bargraph from '../components/Bargraph/Bargraph';
+import { WhiteBalance } from 'expo-camera';
 
 /*@TODOS
     play song from touchable opacity
@@ -15,15 +16,25 @@ import Bargraph from '../components/Bargraph/Bargraph';
     p5.js
 */
 
+/**
+ * Flatlist...
+ * ITEM_MARGIN: change distance between each item
+ * ITEM_SIZE: size of each item
+ */
+
+const ITEM_MARGIN = 0;
+const {width, height} = Dimensions.get('window');
+const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.74;
+const ITEM_SPACING = (width - ITEM_SIZE) / 2;
+
 const ResultPage = ({}) => {
     const {resultsData, setResultsData} = useContext(PlaylistDataContext);
     useEffect(()=> {
         console.log(resultsData)
     })
 
-    //variables for animating flatlist
-    const ITEM_SIZE = Dimensions.get("window").width-70 + 20;
     const scrollX = React.useRef(new Animated.Value(0)).current;
+
     let songCount = 0;
     const commonSongCatcher = (songlist1, songlist2) => { //find common songs; uses songlist1 and songlist2
         const songSet = new Set();
@@ -60,64 +71,80 @@ const ResultPage = ({}) => {
                     <Text style = {{color:"white"}}>{songCount}</Text>
                 </View>
                 <Animated.FlatList 
-                    style={styles.flatlistContainer}
-                    keyExtractor={(item) => item.track.id}
                     data={commonSongLocal}
-                    horizontal={true}
+                    keyExtractor={item => item.track.id.toString()}
+                    horizontal
+                    bounces = {true}
+                    showsHorizontalScrollIndicator = {false}
+                    snapToInterval={ITEM_SIZE+ITEM_MARGIN}
+                    snapToAlignment='start'
+                    decelerationRate={"fast"}
+                    style={styles.flatlistContainer}
+                    contentContainerStyle={{
+                        paddingHorizontal: ITEM_SPACING, //Indents flatlist so first item starts from the center
+                    }}
                     onScroll={Animated.event(
-                        [{nativeEvent:{contentOffset: {x: scrollX}}}],
+                        [{ nativeEvent: { contentOffset: { x: scrollX } } }],
                         {useNativeDriver: true},
                     )}
-                    renderItem={({item, index}) => {
-                        /*@TODO: try making the normal scale of the items super small and only enlarge when its within the 'viewing area' */
-                        const inputRange = [-1, 0, ITEM_SIZE*index, ITEM_SIZE * (index+1.9)];
+                    scrollEventThrottle={16}
+                    renderItem={({item,index}) => {
+                        const inputRange = [
+                            (index - 1) * (ITEM_SIZE+ITEM_MARGIN),
+                            (index) * (ITEM_SIZE+ITEM_MARGIN),
+                            (index + 1) * (ITEM_SIZE+ITEM_MARGIN)
+                        ]
                         const scale = scrollX.interpolate({
                             inputRange,
-                            outputRange: [1, 1, 1, 0],
-                        });
-                        const opacityInputRange = [-1, 0, ITEM_SIZE*index, ITEM_SIZE * (index+1.2), ITEM_SIZE * (index+1.5)];
+                            outputRange: [.3,1,.3],
+                            extrapolate: 'clamp'
+                        })
                         const opacity = scrollX.interpolate({
-                            inputRange: opacityInputRange,
-                            outputRange: [1, 1, 1, .3, 0],
-                        });
-
-                        return <Animated.View style = {{
-                                    maxHeight:'100%',
-
-                                    width:Dimensions.get("window").width - 70,
-                                    maxWidth:Dimensions.get("window").width - 70,
+                            inputRange,
+                            outputRange: [.5,1,.5],
+                            extrapolate: 'clamp'
+                        })
+                        return (
+                            <View style={styles.flatlistItem}>
+                                <Animated.View style={{
+                                    flex: 1,
+                                    alignSelf: "stretch",
+                                    padding: 10,
+                                    justifyContent: "center",
+                                    alignItems: "left",
                                     backgroundColor: "black",
-                                    borderRadius: 7,
-                                    margin: 11,
+                                    borderRadius: 8,
                                     shadowColor: "black",
                                     shadowOffset: {
-                                        width:-3,
+                                        width: -3,
                                         height: 3
-                                    }, 
+                                    },
                                     shadowOpacity: 1,
                                     shadowRadius: 8,
-                                    transform: [{scale}], 
                                     opacity,
-                                    }}
-                                    key={index}>
+                                    transform: [{
+                                        scale
+                                    }]
+                                }}>
                                     <TouchableOpacity style={{
                                         display: "flex",
                                         flexDirection: "row",
-                                        padding: 10,
                                         alignItems: "center",
-                                        justifyContent: 'flex-start',}}>
+                                    }}
+                                    >
                                         <Image
-                                            style={{width: 60, height: 60, marginRight: 20, marginLeft:20, borderRadius: 8}}
-                                            source = {{uri: item.track?.album?.images[0]?.url}}
-                                        />  
+                                            style={{width: 60, height: 60, marginRight: 20, marginLeft: 20, borderRadius: 8}}
+                                            source={{uri: item.track?.album?.images[0]?.url}}
+                                        />
                                         <View>
-                                            <Text style={{color:"white", fontSize: 20}}>{item?.track?.name.substring(0,17) /* @TODO conditional '..' for names that are too long */}</Text> 
+                                            <Text style={{color: "white", fontSize: 20}}>{item?.track?.name.substring(0,17) /* @TODO conditional '..' for names that are too long */}</Text>
                                             <Text style={{color:"white", fontSize: 10}}>{item?.track?.artists[0]?.name}</Text>
                                         </View>
                                     </TouchableOpacity>
                                 </Animated.View>
-                                }
-                    }
+                            </View>
+                        )
+                    }}
                 />
             </View>
             <View style={{flex:1,display:'flex', justifyContent:'center', alignItems:'flex-start', width:'100%', marginLeft: 20}}>
@@ -133,10 +160,10 @@ const ResultPage = ({}) => {
 
 const styles = StyleSheet.create({
     Container: {
-      display: "flex",
+      //display: "flex",
+      flex: 1,
       flexDirection: "column",
       alignItems: 'center',
-      justifyContent:'flex-start',
       width: "100%",
       height: "100%",
       backgroundColor: '#121212',
@@ -150,8 +177,20 @@ const styles = StyleSheet.create({
         marginTop: 13,
     },
     flatlistContainer: {
-      padding: 8,
-      width: Dimensions.get("window").width,
+        flexGrow: 0,
+        height: 100,
+    },
+    flatlistItem: {
+        width: ITEM_SIZE, 
+        alignItems: 'center', 
+        borderRaidus: 7,
+        marginRight: ITEM_MARGIN,
+
+        /**
+         * TO HELP UNDERSTAND HOW THINGS ARE SPACED IN THE FLATLIST
+         * borderWidth: 2,
+         * borderColor: 'white'
+         */
     },
     textfield:{
         fontFamily: 'System',
